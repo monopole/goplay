c1724ae3c213d5a7b8540b4f2c34494178feec27
monopole <jeff.regan@gmail.com>
2023-10-13 09:44:20 -0700

    More notes

--- fruituser/main.go
+++ fruituser/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	// When starting to use the latest (v2) we have to alias v1.
+	applev1 "github.com/monopole/goplay/apple"
+	// The v2 is implicitly stripped here, forcing us (in a good way) to alias the v1 above.
 	"github.com/monopole/goplay/apple/v2"
 	"github.com/monopole/goplay/banana"
 )
 
 func main() {
-	apple.PrintFruit("hello")
+	applev1.PrintFruit()      // must put a v1 here.
+	apple.PrintFruit("hello") // no v2 in package name, yay!  note that the v2 api is incompatible with v1
 	banana.PrintFruit()
-
 }
